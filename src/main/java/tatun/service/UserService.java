e0b6b1598fbf5eba5b0ad167398dd2d64dfac011
Slava <tatun.slava@gmail.com>
2019-01-04 19:10:58 +0600

    add Sprgin security

--- src/main/java/tatun/service/UserService.java
+++ src/main/java/tatun/service/UserService.java
@@ -3,9 +3,13 @@ package tatun.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import tatun.enums.Roles;
+import tatun.model.Role;
 import tatun.model.User;
+import tatun.repository.RoleRepository;
 import tatun.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +21,9 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleService roleService;
 
     public List<User> findAll() {
         return userRepository.findAll();
@@ -48,5 +55,30 @@ public class UserService {
         user.setPassword(passwordHash);
         userRepository.save(user);
         return user;
+    }
+
+    public void createUser(final String login, final String password, Roles roleEnum) {
+        final String passwordHash = passwordEncoder.encode(password);
+
+        User user = userRepository.findByLogin(login);
+        if (user == null) return;
+
+        user = new User();
+        user.setLogin(login);
+        user.setPassword(passwordHash);
+
+        Role role = roleService.findByName(roleEnum.name());
+        if (role == null) {
+            role = new Role();
+            role.setName(roleEnum.name());
+            roleService.save(role);
+        }
+
+        List<Role> roles = new ArrayList<>();
+        roles.add(role);
+
+        user.setRoles(roles);
+
+        userRepository.save(user);
     }
 }
